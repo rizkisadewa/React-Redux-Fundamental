23c67d03456c43daa62b09c9da0dfc29fb35909f
Ari Ajmal <ariajmal.jtk07@gmail.com>
2020-03-22 21:29:44 +0700

    redux fundamental

--- src/index.js
+++ src/index.js
@@ -3,11 +3,56 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import allReducers from './reducers';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+// import { createStore } from 'redux';
+
+// // STORE -> GLOBALIZED STATE 
+
+// // ACTION -> THE ACTION OF DECLARATION 
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
+
+// // REDUCER
+// const counter = (state=0, action) => {
+//   switch(action.type){
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//     default:
+//   }
+// }
+
+// let store = createStore(counter);
+
+// // Display in the console 
+// store.subscribe(() => console.log(store.getState()));
+
+// // DISPATCH 
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
